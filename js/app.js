b77a42277268b45b8674cf5a2038e11b8c1936cf
Hashtechs <omarelewa25@gmail.com>
2021-09-20 20:27:36 +0200

    Style the active link

--- js/app.js
+++ js/app.js
@@ -24,7 +24,16 @@
  * Start Helper Functions
  * 
 */
-
+function style_active_link(element){
+    // Style the active link to make distinguished
+    Object.entries(document.getElementsByClassName("navbar_link")).map(item => {
+        item[1].className = "navbar_link";
+        item[1].parentElement.className = "navbar_item";
+      });
+    
+    element.className = "navbar_link current_nav_anchor";
+    element.parentElement.className = "navbar_item current_nav_link";
+}
 
 
 /**
@@ -51,16 +60,14 @@
 // Build menu 
 document.getElementById("navbar__list").innerHTML = 
         `<li class="navbar_item">
-        <a href="#section1" class="navbar_link">Section 1</a>
+        <a href="#section1" class="navbar_link" onclick="style_active_link(this);">Section 1</a>
         </li>
         <li class="navbar_item">
-        <a href="#section2" class="navbar_link">Section 2</a>
+        <a href="#section2" class="navbar_link" onclick="style_active_link(this);">Section 2</a>
         </li>
         <li class="navbar_item">
-        <a href="#section3" class="navbar_link">Section 3</a>
+        <a href="#section3" class="navbar_link" onclick="style_active_link(this);">Section 3</a>
         `;
-
-
 
 // Scroll to section on link click
 
