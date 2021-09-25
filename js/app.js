1a29027fef663122cb5f0ccaa20965aed239f3bd
Hashtechs <omarelewa21@gmail.com>
2021-09-26 01:39:49 +0200

    Adding new section & Styling Active Section

--- js/app.js
+++ js/app.js
@@ -36,6 +36,17 @@ function style_active_link(element){
 }
 
 
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -60,17 +71,59 @@ function style_active_link(element){
 // Build menu 
 document.getElementById("navbar__list").innerHTML = 
         `<li class="navbar_item">
-        <a href="#section1" class="navbar_link" onclick="style_active_link(this);">Section 1</a>
+        <a href="#section1" class="navbar_link" onclick="style_active_link(this)">Section 1</a>
         </li>
         <li class="navbar_item">
-        <a href="#section2" class="navbar_link" onclick="style_active_link(this);">Section 2</a>
+        <a href="#section2" class="navbar_link" onclick="style_active_link(this)">Section 2</a>
         </li>
         <li class="navbar_item">
-        <a href="#section3" class="navbar_link" onclick="style_active_link(this);">Section 3</a>
+        <a href="#section3" class="navbar_link" onclick="style_active_link(this)">Section 3</a>
+        </li>
+        <li class="navbar_item">
+        <a href="#section4" class="navbar_link" onclick="style_active_link(this)">Section 4</a>
+        </li>
         `;
 
-// Scroll to section on link click
+
 
 // Set sections as active
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+const section4 = document.querySelector('#section4');
 
+document.addEventListener('scroll', function () {
+  if(isInViewport(section1)){
+    let doc = document.getElementsByClassName('landing__container')[0];
+    doc.style.fontSize   = "large";
+
+    document.getElementsByClassName('landing__container')[1].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[2].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[3].style.fontSize = "medium";
+  }
+  else if(isInViewport(section2)){
+    let doc = document.getElementsByClassName('landing__container')[1];
+    doc.style.fontSize   = "large";
+
+    document.getElementsByClassName('landing__container')[0].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[2].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[3].style.fontSize = "medium";
+  }
+  else if(isInViewport(section3)){
+    let doc = document.getElementsByClassName('landing__container')[2];
+    doc.style.fontSize   = "large";
+
+    document.getElementsByClassName('landing__container')[0].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[1].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[3].style.fontSize = "medium";
+  }
+  else if(isInViewport(section4)){
+    let doc = document.getElementsByClassName('landing__container')[3];
+    doc.style.fontSize   = "large";
+
+    document.getElementsByClassName('landing__container')[0].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[1].style.fontSize = "medium";
+    document.getElementsByClassName('landing__container')[2].style.fontSize = "medium";
+  }
+});
 
