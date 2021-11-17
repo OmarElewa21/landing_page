da46b8c545cfd84200d5cd7e7b0829bd2028a9f1
Hashtechs <Hashtechs.stack@gmail.com>
2021-11-17 16:07:07 +0200

    Modifications added

--- js/app.js
+++ js/app.js
@@ -1,22 +1,11 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+ * Global Variables
+ */
 
-/**
- * Define Global Variables
- * 
-*/
+const sections = [].slice.call(document.getElementsByTagName('section'));                  // Array of sections
+const navBar = document.getElementById("navbar__list");                                    // Ul object
+const sectionHeaders = [].slice.call(document.getElementsByClassName('section_header'));   // Section sectionHeaders
+
 
 
 /**
@@ -24,27 +13,52 @@
  * Start Helper Functions
  * 
 */
-function style_active_link(element){
-    // Style the active link to make distinguished
+
+/**
+ * Style the active navigation link
+ * 
+ * @param {*} element
+ * 
+ */
+function styleActiveLink(element){
+    // Adding essential class names to each span nav
     Object.entries(document.getElementsByClassName("navbar_link")).map(item => {
-        item[1].className = "navbar_link";
+        item[1].className = "navbar_link"; 
         item[1].parentElement.className = "navbar_item";
-      });
-    
-    element.className = "navbar_link current_nav_anchor";
-    element.parentElement.className = "navbar_item current_nav_link";
+    });
+
+    element.className = "navbar_link current_nav_anchor";   // Adding current_nav_anchor class name to active span nav
+    element.parentElement.className = "navbar_item current_nav_link";   // Adding current_nav_link class name to active nav li
 }
 
 
+/**
+ * Navigate to the Target Section
+ * 
+ * @param {*} element 
+ */
+function navToSection(element){
+    elementIndex = [].slice.call(navBar.children).indexOf(element.parentElement);   // get nav element index
+    sections[elementIndex].scrollIntoView();        // scroll to secion with the same index
+}
+
+
+/**
+ * Check if element is inside viewport
+ * 
+ * @param {*} element
+ * @returns Boolean
+ */
 function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+    const rect = element.getBoundingClientRect();   // Get element boundaries
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
+
 
 
 /**
@@ -54,100 +68,36 @@ function isInViewport(element) {
 */
 
 // build the nav
+for(let [index, section] of sections.entries()){
+    // Append a nabar bar anchor to each section element
+    let newNavLink = document.createElement("li");
+    newNavLink.classList.add("navbar_item");
+    newNavLink.innerHTML = `<span class="navbar_link">Section ${index+1}</span>`;
+    navBar.appendChild(newNavLink);
 
+    // Add EventListeners to each nav anchor
+    newNavLink.firstChild.addEventListener('click', ($this)=> {
+        styleActiveLink($this.target);  // Style the anchor when active
+        navToSection($this.target);     // Nav to targeted section when clicking the anchor
+    })
+}
 
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
+// Styling Active Section
+document.addEventListener('scroll', ()=> {
+    for(let [index, section] of sections.entries()){
+        if(isInViewport(sectionHeaders[index])){
+            // Style only the active section
+            sectionHeaders.forEach(element => {
+                element.parentElement.style.fontStyle = "normal";     // All containers set font Style to normal
+                element.parentElement.style.fontSize  = "medium";     // All containers set font Size to medium
+            });
+            sectionHeaders[index].parentElement.style.fontStyle  = "italic";      // Set font Style of the View Port Container to italic
+            sectionHeaders[index].parentElement.style.fontSize   = "larger";      // Set font Size of the View Port Container to larger
+        }
+    }
+});
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-document.getElementById("navbar__list").innerHTML = 
-        `<li class="navbar_item">
-        <a href="#section1" class="navbar_link" onclick="style_active_link(this)">Section 1</a>
-        </li>
-        <li class="navbar_item">
-        <a href="#section2" class="navbar_link" onclick="style_active_link(this)">Section 2</a>
-        </li>
-        <li class="navbar_item">
-        <a href="#section3" class="navbar_link" onclick="style_active_link(this)">Section 3</a>
-        </li>
-        <li class="navbar_item">
-        <a href="#section4" class="navbar_link" onclick="style_active_link(this)">Section 4</a>
-        </li>
-        `;
-
-
-
-// Set sections as active
-const section1 = document.getElementsByClassName('section_header')[0];
-const section2 = document.getElementsByClassName('section_header')[1];
-const section3 = document.getElementsByClassName('section_header')[2];
-const section4 = document.getElementsByClassName('section_header')[3];
-
-document.addEventListener('scroll', function () {
-  if(isInViewport(section1)){
-    let doc = document.getElementsByClassName('landing__container')[0];
-    doc.style.fontStyle   = "italic";
-    doc.style.fontSize   = "larger";
-
-    document.getElementsByClassName('landing__container')[1].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[1].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[2].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[2].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[3].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[3].style.fontSize = "medium";
-  }
-  else if(isInViewport(section2)){
-    let doc = document.getElementsByClassName('landing__container')[1];
-    doc.style.fontStyle   = "italic";
-    doc.style.fontSize   = "larger";
-
-    document.getElementsByClassName('landing__container')[0].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[0].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[2].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[2].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[3].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[3].style.fontSize = "medium";
-  }
-  else if(isInViewport(section3)){
-    let doc = document.getElementsByClassName('landing__container')[2];
-    doc.style.fontStyle   = "italic";
-    doc.style.fontSize   = "larger";
-
-    document.getElementsByClassName('landing__container')[0].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[0].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[1].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[1].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[3].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[3].style.fontSize = "medium";
-  }
-  else if(isInViewport(section4)){
-    let doc = document.getElementsByClassName('landing__container')[3];
-    doc.style.fontStyle   = "italic";
-    doc.style.fontSize   = "larger";
-
-    document.getElementsByClassName('landing__container')[0].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[0].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[1].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[1].style.fontSize = "medium";
-
-    document.getElementsByClassName('landing__container')[2].style.fontStyle = "normal";
-    document.getElementsByClassName('landing__container')[2].style.fontSize = "medium";
-  }
-});
-
